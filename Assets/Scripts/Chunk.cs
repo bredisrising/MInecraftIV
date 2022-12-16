b26f718c23d879079a003e8ef0c5cf45b541a4d8
bredisrising <ankitmalhotra788@gmail.com>
2022-12-16 00:11:12 -0800

    work in under way les do this

--- Assets/Scripts/Chunk.cs
+++ Assets/Scripts/Chunk.cs
@@ -6,26 +6,127 @@ public class Chunk : MonoBehaviour
 {
     Mesh mesh;
 
-    [SerializeField] int xSize = 20;
-    [SerializeField] int zSize = 20;
+    MeshRenderer meshRenderer;
+    MeshFilter meshFilter;
 
-    private void Start()
-    {
-        mesh = new Mesh();
-        GetComponent<MeshFilter>().mesh = mesh;
+	//Vector3[] vertices;
+	//int[] triangles;
+	//Vector2[] uvs;
 
-        createShape();
-    }
+	List<Vector3> vertices = new List<Vector3>();
+	List<int> triangles = new List<int>();
+	List<Vector2> uvs = new List<Vector2>();
 
-    void createShape()
-    {
-        mesh.Clear();
-        mesh.vertices = VoxelData.voxelVertices;
-        mesh.triangles = VoxelData.voxelTriangles;
+	bool[,,] voxelMap = new bool[VoxelData.chunkDim, VoxelData.chunkDim, VoxelData.chunkDim];
 
-        
+	private void Start()
+	{
+		meshRenderer = GetComponent<MeshRenderer>();
+		meshFilter = GetComponent<MeshFilter>();
 
-        mesh.RecalculateNormals();
-    }
+		buildChunk();
+
+	}
+
+
+
+	void buildChunk()
+	{
+		makeVoxelMap();
+
+		//vertices = new Vector3[36 * VoxelData.chunkCubeDim * VoxelData.chunkCubeDim * VoxelData.chunkCubeDim];
+		//triangles = new int[36 * VoxelData.chunkCubeDim * VoxelData.chunkCubeDim * VoxelData.chunkCubeDim];
+		//uvs = new Vector2[36 * VoxelData.chunkCubeDim * VoxelData.chunkCubeDim * VoxelData.chunkCubeDim];
+
+		for (int y = 0; y < VoxelData.chunkDim; y++)
+		{
+			for (int x = 0; x < VoxelData.chunkDim; x++)
+			{
+				for (int z = 0; z < VoxelData.chunkDim; z++)
+				{
+					applyVoxelData(new Vector3(x, y, z));
+				}
+			}
+		}
+
+		createMesh();
+	}
+
+
+
+
+	void makeVoxelMap()
+	{
+		for (int y = 0; y < VoxelData.chunkDim; y++)
+		{
+			for (int x = 0; x < VoxelData.chunkDim; x++)
+			{
+				for (int z = 0; z < VoxelData.chunkDim; z++)
+				{
+					voxelMap[x, y, z] = true;
+				}
+			}
+		}
+	}
+
+
+
+
+	bool checkVoxelExists(Vector3 pos)
+	{
+		int x = (int)pos.x;
+		int y = (int)pos.y;
+		int z = (int)pos.z;
+
+		if (x < 0 || x > VoxelData.chunkDim - 1 || y < 0 || y > VoxelData.chunkDim - 1 || z < 0 || z > VoxelData.chunkDim - 1)
+			return false;
+
+		return voxelMap[x, y, z];
+	}
+
+
+
+	void applyVoxelData (Vector3 pos)
+	{
+		// 36 vertices because uv mapping a cube
+		// makes it so each face needs its own 4 vertices
+
+		//triangles use the indices of the vertices
+		//but the vertices are already in order
+		//so using i is fine
+
+		for (int face = 0; face < 6; face++)
+		{
+
+			if(!checkVoxelExists(pos + VoxelData.faceChecks[face])) // adds face checks to see if the face is facing a voxel or not
+			{
+				for (int vert = 0; vert < 6; vert++)
+				{
+					vertices.Add(VoxelData.voxelVertices[VoxelData.voxelTriangles[face * 6 + vert]] + pos);
+					triangles.Add(vertices.Count - 1); //using i is fine
+					uvs.Add(VoxelData.voxelUVs[vert]);
+				}
+			} 
+			
+		}
+	}
+
+
+
+
+	void createMesh()
+	{
+
+		mesh = new Mesh();
+		
+
+		mesh.vertices = vertices.ToArray();
+		mesh.triangles = triangles.ToArray();
+		mesh.uv = uvs.ToArray();
+
+		mesh.RecalculateNormals();
+
+		meshFilter.mesh = mesh;
+	}
 
 }
--- Assets/Scripts/VoxelData.cs
+++ Assets/Scripts/VoxelData.cs
@@ -4,9 +4,13 @@ using UnityEngine;
 
 public class VoxelData
 {
+    public static readonly int chunkDim = 30;
+
+    public static readonly int verticesInCube = 36;
+
     public static readonly Vector3[] voxelVertices = new Vector3[8]
     {
-        new Vector3(0,0,0),
+        new Vector3(0,0,0), 
         new Vector3(1,0,0),
         new Vector3(0,1,0),
         new Vector3(0,0,1),
@@ -19,12 +23,36 @@ public class VoxelData
 
     public static readonly int[] voxelTriangles = new int[36]
     {
-        0, 2, 1, 1, 2, 4, //front face
+        0, 2, 4, 4, 1, 0, //front face
         6, 7, 5, 5, 3, 6, //bak face
-        4, 2, 5, 5, 7, 4, //top feces
+        2, 5, 7, 7, 4, 2, //top feces
         1, 6, 3, 3, 0, 1, //bottom feces
         1, 4, 7, 7, 6, 1, //right feces
         3, 5, 2, 2, 0, 3, //left face
 
     };
+
+    public static readonly Vector3[] faceChecks = new Vector3[6]
+    {
+        //corresponds with order of voxel triangle faces above
+        new Vector3(0,0,-1),
+        new Vector3(0,0,1),
+        new Vector3(0,1,0),
+        new Vector3(0,-1,0),
+        new Vector3(1,0,0),
+        new Vector3(-1,0,0),
+    };
+
+    public static readonly Vector2[] voxelUVs = new Vector2[6]
+	{
+        new Vector2(0f, 0f),
+        new Vector2(0f, 1f),
+        new Vector2(1f, 1f),
+        new Vector2(1f, 1f),
+        new Vector2(1f, 0f),
+        new Vector2(0f, 0f),
+    };
+
+    
+
 }
