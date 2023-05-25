82cbc85d0acea995cfe862c8ce8cbedbbbbfe3d4
bredisrising <ankitmalhotra788@gmail.com>
2023-05-25 14:07:43 -0700

    debuggng

--- Assets/Scripts/DoOver/NewWorld.cs
+++ Assets/Scripts/DoOver/NewWorld.cs
@@ -24,7 +24,7 @@ public class NewWorld : MonoBehaviour
 	void Start()
     {
 		player.position = new Vector3(2 * 16, 20, 2 * 16);
-		initialGeneration(10);
+		initialGeneration(30);
 		lastChunkPositionOfPlayer = getChunkPosFromWorldPos(player.position);
 		index = 5;
 	}
@@ -105,40 +105,48 @@ public class NewWorld : MonoBehaviour
 		int initSize = size;
 		int[] blockMap = new int[4096 * initSize];
 
+		startTime = Time.realtimeSinceStartup;
+
 		ComputeBuffer blockMapCB = new ComputeBuffer(initSize * 4096, sizeof(int));
 		blockMapCB.SetData(blockMap);
 
 		ComputeBuffer chunkPositionsCB = new ComputeBuffer(8 * VoxelData.cubicWorldSize + 10, sizeof(int) * 3);
 		chunkPositionsCB.SetData(chunkPositions);
 
-
+		
 
 		terrainGenShader.SetBuffer(0, "positions", chunkPositionsCB);
 		terrainGenShader.SetBuffer(0, "outputBuffer", blockMapCB);
 
 		terrainGenShader.Dispatch(0, initSize * 16, 1, 1);
 
-		
+		endTime = Time.realtimeSinceStartup;
+		Debug.Log("allocate and dispatch shader" + (endTime - startTime) * 1000);
+
+
 
 		startTime = Time.realtimeSinceStartup;
 
 		blockMapCB.GetData(blockMap);
 
 		endTime = Time.realtimeSinceStartup;
-		Debug.Log("allocate and dispatch shader" + (endTime - startTime) * 1000);
+		Debug.Log("get data time" + (endTime - startTime) * 1000);
 
 		blockMapCB.Dispose();
 		chunkPositionsCB.Dispose();
 
-		
-
-		
 
 
-		for(int index = 0; index < initSize; index++)
+
+
+		startTime = Time.realtimeSinceStartup;
+		for (int index = 0; index < initSize; index++)
 		{
 			activeChunks[chunkPositions[index]] = StaticChunkRendering.GenerateChunk(blockMap, chunkPositions[index] * 16, worldMaterial, index);
 		}
+		endTime = Time.realtimeSinceStartup;
+
+		Debug.Log("render time" + (endTime - startTime) * 1000);
 	}
 
 
